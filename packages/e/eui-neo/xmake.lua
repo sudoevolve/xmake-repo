1cbe65175ff6f5756552d434a30a9b66371c7aa3
Admin <admin@localhost>
2026-09-02 17:44:47 +0800

    chore: track EUI development package separately

--- packages/e/eui-neo/xmake.lua
+++ packages/e/eui-neo/xmake.lua
@@ -3,10 +3,12 @@ package("eui-neo")
     set_description("Cross-platform, high-performance, low-overhead C++17 GPUI framework")
     set_license("Apache-2.0")
 
-    add_urls("https://github.com/sudoevolve/EUI-NEO/archive/refs/tags/$(version).tar.gz",
-             "https://github.com/sudoevolve/EUI-NEO.git")
+    add_urls("https://github.com/sudoevolve/EUI-NEO/archive/refs/tags/$(version).tar.gz")
+    add_urls("https://github.com/sudoevolve/EUI-NEO.git", {alias = "git", submodules = false})
 
-    add_versions("v0.5.8", "ca886cfb62bc05a849d2176bd6b30bbf2d0e14e1f866305ce622af6177548c8c")
+    -- Development snapshot: the git: prefix selects the git URL alias, while
+    -- consumers request the Windows-safe version name "dev".
+    add_versions("git:dev", "dev")
     add_versions("v0.5.7", "2d3ec0a36e34b98d13dbdaf67afa4fe178cb4b52841eb17529517cb48be43551")
     add_versions("v0.5.5", "cf0da91d7544fe406b704922137fd4d55ed080b3e647501e0ca5303abb00eb98")
 
